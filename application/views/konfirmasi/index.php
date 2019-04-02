4a5a1c757bd499395e21c8c622b988f2ea4fe49d
fadhfaiz <faadh.faaiz@gmail.com>
2019-04-02 16:16:01 +0700

    update 1

---
+++ application/views/konfirmasi/index.php
@@ -0,0 +1,21 @@
+<div class="container alert alert-danger mt-5" role="alert">
+  Masih dalam tahap pengembangan! Ini belum jadi.
+</div>
+
+<form class="container mt-3">
+	<div class="form-group">
+		<label for="exampleInputEmail1">Email address</label>
+		<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+			placeholder="Enter email">
+		<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+	</div>
+	<div class="form-group">
+		<label for="exampleInputPassword1">Password</label>
+		<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+	</div>
+	<div class="form-group">
+		<label for="exampleFormControlFile1">Example file input</label>
+		<input type="file" class="form-control-file" id="exampleFormControlFile1">
+	</div>
+	<button type="submit" class="btn btn- btn-primary">Submit</button>
+</form>
---
+++ application/views/marketplace/detail_product.php
@@ -0,0 +1,17 @@
+<section class="container">
+	<div class="row">
+		<div class="col-lg-6 col-md-12 col-sm-12 mt-4 mb-4">
+			<img src="<?php echo base_url()?>assets/img/sansiviera.jpg" class="img-fluid" alt="Responsive image">
+		</div>
+
+		<div class="col-lg-6 col-md-12 col-sm-12 mt-4 mb-4">
+			<div class="text-center"><h1>Tanaman Sansiveera</h1></div>
+			<div class="text-center"<a class="h4">Rp 360.000</a></div>
+			<a href="<?= base_url(); ?>marketplace/index" class="btn btn-block btn-sm btn-outline-info my-3"><i class="fas fa-arrow-circle-left mr-2">
+				</i> Kembali ke Marketplace</a>
+			<hr>
+
+			<a class="h5">Detail Produk :</a>
+		</div>
+	</div>
+</section>
