70bcec11afe66782086d79c189d5dd42b6542811
IDestiyanti <intandesti25@gmail.com>
2019-04-03 14:48:57 +0700

    update care

--- application/views/description/kaktus.php
+++ application/views/description/kaktus.php
@@ -21,8 +21,9 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-lg-4 ">
-                  <img src="<?php echo base_url()?>assets/img/des.png" alt="Responsive image">
+                <div class="col-lg-1"></div>
+                <div class="col-lg-3 col-md-12 col-sm-12 mt-4 mb-4">
+                  <img src="<?php echo base_url()?>assets/img/care1.png" alt="Responsive image">
                 </div>
                 <div class="col-lg-4 col-md-12 col-sm-12 mt-4 mb-4">
                 <h5 style="font-weight:bold">How to Care ?</h5>
